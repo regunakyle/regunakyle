eea18883ef702278deb25f5d1c2a65aac725d3f7
Reguna <60984878+regunakyle@users.noreply.github.com>
2025-05-26 00:40:07 +0800

    update script and headers

--- redirect_404.js
+++ redirect_404.js
@@ -2,10 +2,17 @@
 // 404 is bad for SEO
 // TODO: Figure out a smart method to redirect from the main branch instead of hack like this
 const parsedUrl = new URL(window.location.href);
+const fullPathname = `${parsedUrl.pathname.substring(11)}${parsedUrl.search}`;
+
+// If somehow not redirected, set the href of the link to the correct path
+document.addEventListener("DOMContentLoaded", () => {
+  const link = document.getElementById("redirect-link");
+  if (link) {
+    link.href = `https://www.ericleung.dev${fullPathname}`;
+  }
+});
 
 window.location.replace(
   // "/regunakyle" is 11 characters long
-  `https://www.ericleung.dev${parsedUrl.pathname.substring(11)}${
-    parsedUrl.search
-  }`
+  `https://www.ericleung.dev${fullPathname}`
 );
